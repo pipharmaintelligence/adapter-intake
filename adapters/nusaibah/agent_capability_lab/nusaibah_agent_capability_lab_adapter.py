26d89029780fc216b784854f47325531b82aa45e
ASUS ROG <n.aljaloudi@gmail.com>
2026-09-22 12:51:19 +0300

    Bump capability lab to 0.1.7 for Agent metadata correction

--- adapters/nusaibah/agent_capability_lab/nusaibah_agent_capability_lab_adapter.py
+++ adapters/nusaibah/agent_capability_lab/nusaibah_agent_capability_lab_adapter.py
@@ -567,7 +567,7 @@ class NusaibahAgentCapabilityLabAdapter(Adapter):
     """
 
     key: ClassVar[str] = "nusaibah.agent_capability_lab"
-    version: ClassVar[str] = "0.1.6"
+    version: ClassVar[str] = "0.1.7"
 
     def invoke(
         self,
